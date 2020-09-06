caa938da35cd5aadeb2b76ff5be526a6a8709c40
Nolan Sedley <nsedley@gmail.com>
2020-09-06 13:05:17 -0700

    Retrieve study metadata

--- pages/studies/[slug].js
+++ pages/studies/[slug].js
@@ -1,17 +1,22 @@
 import { useRouter } from 'next/router'
 import Layout from "../../components/layout";
 import { getStudyBySlug, getAllStudies } from "../../lib/studies";
-import { Row } from "react-bootstrap";
+import { Container, Row as div } from "react-bootstrap";
 
-export default function Study({ study }) {
+export default function Study({ study = { slug, book, chapter, suffix, content } }) {
   const router = useRouter()
-
   if (!router.isFallback && !study?.slug) {
     return <ErrorPage statusCode={404} />
   }
   return (
-    <Layout>      
-      <Row className="studyContent" dangerouslySetInnerHTML={{ __html: study.content }}></Row>
+    <Layout>
+      <Container>
+        <div>
+          Study:&nbsp;<a href={"/books#" + study.book}>{study.book}</a> > <a href={"#"}>{study.chapter}</a>
+        </div>
+        <hr />
+        <div dangerouslySetInnerHTML={{ __html: study.content }}></div>
+      </Container>
     </Layout>
   )
 }
@@ -28,7 +33,7 @@ export async function getStaticProps({ params }) {
 }
 
 export async function getStaticPaths() {
-  const studies = getAllStudies(['slug'])  
+  const studies = getAllStudies(['slug'])
   return {
     paths: studies.map((s) => {
       return {
