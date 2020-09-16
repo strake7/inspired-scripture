55bbd056c2345607ae984dbd1479afeeed35d2eb
Nolan Sedley <nsedley@gmail.com>
2020-09-15 22:41:59 -0700

    Add basic about page

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import Layout from "../components/layout";
+import { Container } from "react-bootstrap";
+
+export default function About(props) {
+return(
+  <Layout>
+    <Container>
+      <h5 className="border-bottom pb-4 pt-3">About</h5>
+      <p>Thank you for your interest in <a href="https://inspiredscripture.com">inspiredscripture.com</a>. More information is coming soon about this site.</p>
+      <p>This site is openly developed. If you have corrections and enhancements you would like to make please see <a href="https://github.com/strake7/inspired-scripture" target="_blank">the repository on GitHub</a>.</p>
+    </Container>
+  </Layout>
+)
+}
