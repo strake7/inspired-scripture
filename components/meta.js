ce8ea2b5154b5ecfce4681b3cae381da1fa7ad6e
Nolan Sedley <nsedley@gmail.com>
2020-09-16 01:34:17 -0700

    Add default title meta

--- components/meta.js
+++ components/meta.js
@@ -2,7 +2,8 @@ import Head from 'next/head'
 
 export default function Meta() {
   return (
-    <Head>
+    <Head>      
+      <title>Inspired Scripture</title>
       <meta name="description" content="Learn and study about the relevance of the Old Testament to modern life as a believer in Jesus Christ."></meta>
     </Head>
   )
