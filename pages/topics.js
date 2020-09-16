7b48b08aa97498fb41b7c1bcd9b009de3a1ecebb
Nolan Sedley <nsedley@gmail.com>
2020-09-16 01:41:27 -0700

    Properly align under construction message

--- pages/topics.js
+++ pages/topics.js
@@ -1,9 +1,12 @@
 import Layout from "../components/layout"
+import { Container } from "react-bootstrap"
 
 function Topics() {
   return (
     <Layout>
-      <div>Under construction. See our <a href="/books">studies by book page</a> in the meantime!</div>
+      <Container>
+        Under construction. See our <a href="/books">studies by book page</a> in the meantime!
+      </Container>      
     </Layout>
   )
 }
