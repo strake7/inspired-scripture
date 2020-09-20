f6c913b422a896ec6a49cde1e3ee823e9347a0e2
Nolan Sedley <nsedley@gmail.com>
2020-09-20 16:16:05 -0700

    Add underline style to fix study view

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
+import '../styles.css'
 
 export default function InspiredScriptureApp({ Component, pageProps }) {
   return <Component {...pageProps} />
