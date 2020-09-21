a736946c2a757f0e838c6f83ca962eacb5c1b7cf
Nolan Sedley <nsedley@gmail.com>
2020-09-20 23:31:23 -0700

    Initial topics view

--- pages/topics.js
+++ pages/topics.js
@@ -1,14 +1,42 @@
 import Layout from "../components/layout"
 import { Container } from "react-bootstrap"
-
-function Topics() {
+import { getAllTopics } from "../lib/topics";
+function Topics({ allTopics }) {
   return (
     <Layout>
       <Container>
         Under construction. See our <a href="/books">studies by book page</a> in the meantime!
-      </Container>      
+        <ol>
+          {allTopics.map((topic) => (
+            <li>{topic.name}
+              <ol>
+                {topic.sections.map((section) =>
+                  (
+                    <li>{section.name}
+                      <ol>
+                        {section.studies.map((study) => (
+                          <li><a href={`/studies/${study.slug}`} target="_blank">{study.name}</a></li>
+                        ))}
+                      </ol>
+                    </li>
+                  )
+                )}
+              </ol>
+            </li>
+          ))}
+        </ol>
+      </Container>
     </Layout>
   )
 }
 
 export default Topics
+
+export async function getStaticProps({ params }) {
+  const allTopics = await getAllTopics()
+  return {
+    props: {
+      allTopics
+    }
+  }
+}
