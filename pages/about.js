0782699dfc3ec6181ba97d33fee21979c5f704e1
Nolan Sedley <nsedley@gmail.com>
2020-09-28 00:02:03 -0700

    Refresh study documents with latest

--- pages/about.js
+++ pages/about.js
@@ -7,7 +7,7 @@ return(
     <Container>
       <h5 className="border-bottom pb-4 pt-3">About</h5>
       <p>Thank you for your interest in <a href="https://inspiredscripture.com">inspiredscripture.com</a>. More information is coming soon about this site.</p>
-      <p>This site is openly developed. If you have corrections and enhancements you would like to make please see <a href="https://github.com/strake7/inspired-scripture" target="_blank">the repository on GitHub</a>.</p>
+      <p>This site is developed openly. If you have corrections and enhancements you would like to report or make please see <a href="https://github.com/strake7/inspired-scripture" target="_blank">the repository on GitHub</a>.</p>
     </Container>
   </Layout>
 )
