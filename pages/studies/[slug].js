922ac7f548320e6c0839143204ab03c9f8fe61c3
Nolan Sedley <nsedley@gmail.com>
2020-10-11 13:33:37 -0700

    Fix study page title suffix

--- pages/studies/[slug].js
+++ pages/studies/[slug].js
@@ -12,7 +12,7 @@ export default function Study({ study = { slug, book, chapter, suffix, content }
     <Layout>
       <Container>
         <h5 className="border-bottom block pb-2 pt-3">
-          Study:&nbsp;<a href={"/books#" + study.book}>{study.book}</a> > <a href={"#"}>{study.chapter}{study.suffix}</a>
+          Study:&nbsp;<a href={"/books#" + study.book}>{study.book}</a> / <a href={"#"}>{study.chapter}&nbsp;{study.suffix}</a>
         </h5>        
         <div dangerouslySetInnerHTML={{ __html: study.content }}></div>
       </Container>
