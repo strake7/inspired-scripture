1c26ae86aef1e4c5450b07eb0466211b6311782e
Nolan Sedley <nsedley@gmail.com>
2022-01-15 21:16:22 -0800

    Use "Bible" on study page

--- pages/studies/[slug].js
+++ pages/studies/[slug].js
@@ -15,7 +15,7 @@ export default function Study({ study = { slug, book, chapter, suffix, content }
         {
           <div className="d-print-none">
             <Heading >
-              Study:&nbsp;<a href={"/books#" + study.book}>{study.book}</a> / <a href={"#"}>{study.chapter}&nbsp;{study.suffix}</a>
+              Bible Study:&nbsp;<a href={"/books#" + study.book}>{study.book}</a> / <a href={"#"}>{study.chapter}&nbsp;{study.suffix}</a>
               <a href="#" onClick={() => window.print()}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-printer" viewBox="0 0 16 16">
                   <path d="M2.5 8a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z" />
