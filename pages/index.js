efd9e380770dfa21fb663cdf6e01d7e4e78b8c02
Nolan Sedley <nsedley@gmail.com>
2022-05-30 11:38:05 -0700

    Move about text and update favicon

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import Heading from '../components/heading';
 import Link from 'next/link';
 
 const Home = () => (
-  <Layout meta={{ title: "Inspired Scripture", description: "Learn and study about the relevance of the Old Testament to modern life as a believer in Jesus Christ."}}>
+  <Layout meta={{ title: "Inspired Scripture", description: "Learn and study about the relevance of the Old Testament to modern life as a believer in Jesus Christ." }}>
     <Jumbotron className="text-white" style={{ position: 'relative', backgroundSize: 'cover', backgroundImage: 'url(/welcome-span.jpg)', backgroundPosition: 'center' }}>
       <div style={{ position: 'absolute', top: 0, bottom: 0, right: 0, left: 0, background: '#000', opacity: 0.5 }}></div>
       <Container className='text-center text-light' style={{ position: 'relative' }}>
@@ -16,18 +16,7 @@ const Home = () => (
     </Jumbotron>
     <Container id='content'>
       <Heading>Welcome to Inspired Scripture</Heading>
-      <p>The purpose of this web site is to equip and encourage believers in Christ to see the relevance of the Old Testament to modern life. Under the inspiration of the Holy Spirit, Paul revealed that every single book of the Old Testament is both “inspired” and also “profitable for teaching, for reproof, for correction, for training in righteousness;” (2 Tim. 3:16). Most believers would profess that the Old Testament is “inspired.” Yet, few would use a book like Leviticus, Numbers or Deuteronomy for “for teaching, for reproof, for correction, for training in righteousness.”</p>
-      <p>Believers in Christ are correctly taught that He fulfilled all of the tests for salvation. Yet, believers are rarely taught how the Old Testament can still be relevant to their walk for reasons unrelated to salvation. Although the list is not all inclusive, these studies seek show how God’s Law and the Old Testament explain how to:</p>
-      <ol>
-        <li>Walk in fellowship with Jesus after finding salvation with Him</li>
-        <li>Maintain God’s standards of holiness as a believer</li>
-        <li>Receive the fullness of Jesus’ intended blessings</li>
-        <li>Be His salt in the wound of sin</li>
-        <li>Trust in God’s sovereignty and faithfulness</li>
-        <li>Defend the Word and having faith in it to share it with others</li>
-        <li>Establish the right relationships and church structure to ensure growth and accountability</li>
-      </ol>
-      <p>Most chapters are separately organized into at least seven lessons. Based upon Paul’s teachings, these studies adopt both a historical and simultaneous allegorical approach to the Old Testament wherever possible (Gal. 4:24). These studies also quote from the New American Standard Bible. </p>
+      <p className='mb-4'>This is a place of study with content intended to equip and encourage you as a believer in Christ to see the relevance of the Old Testament in modern life. Studies are organized into at least seven lessons. Based upon Paul’s teachings, these studies adopt both a historical and simultaneous allegorical approach to the Old Testament wherever possible (Gal. 4:24). These studies also quote from the New American Standard Bible.</p>
       <hr />
       <Row>
         <Col xs={6} className="text-center">
