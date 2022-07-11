23eab16c6161852d7d8739646c5afa03e27a5670
Nolan <strake7@users.noreply.github.com>
2022-07-10 23:16:44 -0700

    Change bible study/topic pathnames to lowercase (#51)
    * Account for lowercase slugs
    
    * Fix slackers
    
    * Mv studies to lowercase naming

--- _studies/converter_fns.sh
+++ _studies/converter_fns.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 # description: Helper bash functions for converting study documents
-# use: source ./converter_fns.sh 
+# use: source ./converter_fns.sh
 #   convert_all_docx
 
 # for any docx file within the _studies directory, convert to html using pandoc
 
 function get_convert_cmd {
-  fhtml="${1%%.docx}.html" # append html ext           
-  fhtml="${fhtml// /-}" # replace space with dash
+  fhtml="${1%%.docx}.html"                             # append html ext
+  fhtml="${fhtml// /-}"                                # replace space with dash
+  fhtml=$(echo "$fhtml" | awk '{ print tolower($1) }') # lowercase name for slugs
   tmp="tmp-$fhtml"
-  echo "pandoc --self-contained --metadata pagetitle='temporary' \"$1\" -o $tmp && pandoc $tmp -o \"$fhtml\"" # we need a standalone html file first to get images encoded as a raw data, then we go back to a shortened html document.  
+  echo "pandoc --self-contained --metadata pagetitle='temporary' \"$1\" -o $tmp && pandoc $tmp -o \"$fhtml\"" # we need a standalone html file first to get images encoded as a raw data, then we go back to a shortened html document.
 }
 
 function convert_all_docx {
-  echo `tput setaf 3`Starting conversion of docx files to html...`tput sgr0`     
-  for fdocx in *.docx
-  do    
+  echo $(tput setaf 3)Starting conversion of docx files to html...$(tput sgr0)
+  for fdocx in *.docx; do
     cmd=$(get_convert_cmd "$fdocx")
-    echo `tput setaf 4`Running command $cmd`tput sgr0`      
+    echo $(tput setaf 4)Running command $cmd$(tput sgr0)
     eval $cmd &
   done
   wait
@@ -26,19 +26,18 @@ function convert_all_docx {
 }
 
 function convert_single_docx {
-  echo `tput setaf 3`Starting conversion of $1 files to html...`tput sgr0`    
+  echo $(tput setaf 3)Starting conversion of $1 files to html...$(tput sgr0)
   cmd=$(get_convert_cmd "$1")
-  echo `tput setaf 4`Running command $cmd`tput sgr0`      
+  echo $(tput setaf 4)Running command $cmd$(tput sgr0)
   eval $cmd
   echo "Cleaning up..."
   rm tmp-**.html
 }
 
 function remove_chapter_from_name {
-  for f in *-Chapter-*.html
-  do        
-    echo `tput setaf 4`Cleaning '-Chapter-' from $f`tput sgr0`      
-    mv "$f" "${f/-Chapter-/-}"    
+  for f in *-Chapter-*.html; do
+    echo $(tput setaf 4)Cleaning '-Chapter-' from $f$(tput sgr0)
+    mv "$f" "${f/-Chapter-/-}"
   done
   wait
   echo "Done"
