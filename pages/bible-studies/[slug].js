ca053870aa1cd47310a04cd138c4294869ed1b0a
Nolan <strake7@users.noreply.github.com>
2022-07-10 21:02:39 -0700

    Add Job study videos and 1 Kings subheaders (#50)
    * 1 Kings subheaders
    
    * Add Job study videos
    
    * Iframe title

--- pages/bible-studies/[slug].js
+++ pages/bible-studies/[slug].js
@@ -11,23 +11,30 @@ export default function Study({ study = { slug, book, chapter, suffix, content }
     return <ErrorPage statusCode={404} />
   }
   const studyName = `${study.book} ${study.chapter} ${study.suffix}`
+  const renderStudyVideo = () =>
+    study.videoSrc
+      ? (
+        <div className="video">
+          <iframe width="560" height="315" src={study.videoSrc} title={study.slug + " Study Video"} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+        </div>
+      )
+      : null;
+
   return (
     <Layout meta={{ title: `Bible Study: ${studyName}`, description: `John Edson's commentary on ${studyName} in a seven fold outline.` }}>
       <Container className="study-content">
-        {
-          <div className="d-print-none">
-            <Heading >
-              Bible Study:&nbsp;<a href={"/bible-studies#" + study.book}>{study.book}</a> / <a href={"#"}>{study.chapter}&nbsp;{study.suffix}</a>
-              <a href="#" onClick={() => window.print()}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-printer" viewBox="0 0 16 16">
-                  <path d="M2.5 8a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z" />
-                  <path d="M5 1a2 2 0 0 0-2 2v2H2a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h1v1a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2v-1h1a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-1V3a2 2 0 0 0-2-2H5zM4 3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v2H4V3zm1 5a2 2 0 0 0-2 2v1H2a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-1v-1a2 2 0 0 0-2-2H5zm7 2v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1z" />
-                </svg>
-              </a>
-            </Heading>
-          </div>
-
-        }
+        <div className="d-print-none">
+          <Heading >
+            Bible Study:&nbsp;<a href={"/bible-studies#" + study.book}>{study.book}</a> / <a href={"#"}>{study.chapter}&nbsp;{study.suffix}</a>
+            <a href="#" onClick={() => window.print()}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-printer" viewBox="0 0 16 16">
+                <path d="M2.5 8a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z" />
+                <path d="M5 1a2 2 0 0 0-2 2v2H2a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h1v1a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2v-1h1a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-1V3a2 2 0 0 0-2-2H5zM4 3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v2H4V3zm1 5a2 2 0 0 0-2 2v1H2a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-1v-1a2 2 0 0 0-2-2H5zm7 2v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1z" />
+              </svg>
+            </a>
+          </Heading>
+          {renderStudyVideo()}
+        </div>
         <div dangerouslySetInnerHTML={{ __html: study.content }}></div>
       </Container>
     </Layout>
