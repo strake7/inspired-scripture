7868741af744d4814ddef25ecde852b20f16bda1
Nolan Sedley <nsedley@gmail.com>
2023-04-23 22:51:25 -0700

    Update nextjs and use middle-ware to forece lowercase pathnames

--- next.config.js
+++ next.config.js
@@ -11,6 +11,16 @@ module.exports = {
         destination: '/#bible-studies',
         permanent: true,
       },
+      {
+        source: '/studies',
+        destination: '/#topics',
+        permanent: true,
+      },
+    {
+        source: '/studies/:slug*',
+        destination: '/bible-studies/:slug*',
+        permanent: true,
+      },
     ]
   },
 }
