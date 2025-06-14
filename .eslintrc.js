1a4da1c8a20474b8d4caaeb343ed7c537d92309f
Nolan Sedley <strake7@users.noreply.github.com>
2025-06-13 23:12:28 -0700

    feat: upgrade eslint (#133)
    * feat: upgrade eslint
    
    * fix: use exports

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: ["next", "next/core-web-vitals", "prettier"],
-  plugins: ["prettier"],
-  rules: {
-    "prettier/prettier": "error",
-    "react/no-unescaped-entities": 0
-  },
-}
--- next.config.js
+++ next.config.js
@@ -1,27 +1,3 @@
 module.exports = {
   output: 'export',
-  async redirects() {
-    return [
-      {
-        source: '/topics',
-        destination: '/#topics',
-        permanent: true,
-      },
-      {
-        source: '/bible-studies',
-        destination: '/#bible-studies',
-        permanent: true,
-      },
-      {
-        source: '/studies',
-        destination: '/#topics',
-        permanent: true,
-      },
-      {
-        source: '/studies/:slug*',
-        destination: '/bible-studies/:slug*',
-        permanent: true,
-      },
-    ]
-  },
 }
