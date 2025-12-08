3f54cc9117944fec1753267449475776a54405c3
Nolan Sedley <strake7@users.noreply.github.com>
2025-12-07 20:38:07 -0800

    feat: bump nextjs (#143)
    * feat: bump nextjs
    
    * chore: move to eslint

--- middleware.js
+++ middleware.js
@@ -1,9 +1,11 @@
-import { NextResponse } from 'next/server';
+import { NextResponse } from 'next/server'
 
 export function middleware(req) {
   // force lower-case match and try redirect if needed
   if (req.nextUrl.pathname === req.nextUrl.pathname.toLowerCase())
-    return NextResponse.next();
+    return NextResponse.next()
 
-  return NextResponse.redirect(new URL(req.nextUrl.pathname.toLowerCase(), req.nextUrl.origin));
+  return NextResponse.redirect(
+    new URL(req.nextUrl.pathname.toLowerCase(), req.nextUrl.origin),
+  )
 }
